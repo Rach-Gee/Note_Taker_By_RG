4dc84908c7bf3f6da92168c7c9eb48ef59584962
Rach Gee <Rach-Gee@hotmail.com>
2021-08-13 19:13:07 +0800

    adding folders with working Web links

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require("express");
+const express = require('express');
 const path = require('path')
 const app = express();
+const webRouter = require('./routes/web')
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('Develop/public'));
+
+app.use(webRouter);
+
+
+app.listen(PORT, () => {
+    console.log('Now Listing on http://localhost:' + PORT)
+})
+
 
 
